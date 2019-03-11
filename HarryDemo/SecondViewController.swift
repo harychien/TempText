3893afcf3478320957e899cbe4685c3b5102d0e6
Harry <harychien@gmail.com>
2019-03-11 15:04:31 +0800

    Initial Commit

---
+++ HarryDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HarryDemo
+//
+//  Created by DGA2 on 2019/3/11.
+//  Copyright © 2019年 doublegreen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
